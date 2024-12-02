a708f0f1d87a22bf0e330bfad960fe937caa9274
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-12-02 12:15:12 +0500

    Update page.tsx

--- demo-next/app/page.tsx
+++ demo-next/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div>Page</div>;
+import { getTodos } from "@/actions/todo";
+import { TodoForm } from "./TodoForm";
+import { TodoEntryEditButton } from "./TodoEntryEditButton";
+
+export default async function Page() {
+  const todos = await getTodos();
+
+  return (
+    <div>
+      <h1>Todo list</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text}
+            <TodoEntryEditButton todo={todo} />
+          </li>
+        ))}
+      </ul>
+      <TodoForm />
+    </div>
+  );
 }
